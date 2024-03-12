1d42e66336c3a832405b9f0219c59a8aacea02e3
oddish <wjhgeneral@outlook.com>
2024-03-12 10:21:58 +0800

    20240312

---
+++ cuda/math_functions.cu
@@ -0,0 +1,3 @@
+// Copyright (c) CNIC. 2024. All rights reserved.
+// Author: Jiaheng Wang
+// Date: 2024-03-12
---
+++ cuda/math_functions.h
@@ -0,0 +1,8 @@
+// Copyright (c) CNIC. 2024. All rights reserved.
+// Author: Jiaheng Wang
+// Date: 2024-03-12
+
+#ifndef CUDALICE_MATH_FUNCTIONS_H
+#define CUDALICE_MATH_FUNCTIONS_H
+
+#endif  // CUDALICE_MATH_FUNCTIONS_H
